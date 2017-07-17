107844e4a3f80824db0ef6fff6f1ac60a25d09f9
Your Name <informatica@mobba.es>
2017-07-17 03:46:59 +0200

    Initial commit

--- app/src/main/java/com/xavicortes/xclapp/fragments/Musica.java
+++ app/src/main/java/com/xavicortes/xclapp/fragments/Musica.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import static android.content.Context.BIND_AUTO_CREATE;
 
 /*
-Controla un boudservice el que hi ha un mediaplayer executant una canço
+Controla un boudservice on hi ha un mediaplayer executant una canço
 */
 public class Musica
         extends Fragment
